65947ba122de2e30da7734f6b06ec09346f45a70
Samuel Jeune <sam-j@hotmail.fr>
2016-01-13 15:18:33 +0100

    mise a jour edible

--- ProjetRPG/src/item/Edible.java
+++ ProjetRPG/src/item/Edible.java
@@ -1,9 +1,22 @@
 package item;
 
+import action.Effect;
+import character.Caracteristics;
+
 public class Edible extends Item {
-    private char   caract;
+    private Caracteristics   caract;
     private int  edibleValue;
+    private boolean permanent;
+
+    public Edible(Caracteristics caract, int edibleValue, boolean permanent) {
+        this.caract = caract;
+        this.edibleValue = edibleValue;
+        this.permanent = permanent;
+    }
+
     
+    
+     Effect effect = new Effect(caract,edibleValue , permanent);
     
     
 }
