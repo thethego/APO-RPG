3cb813bda00426c1898822a86cb93cfb222ef196
Samuel Jeune <sam-j@hotmail.fr>
2016-01-20 12:11:45 +0100

    10 combat dans mainquest

--- ProjetRPG/src/projectrpg/Quest.java
+++ ProjetRPG/src/projectrpg/Quest.java
@@ -10,6 +10,9 @@ import controler.ControlerIA;
 import item.Armor;
 import item.Item;
 import item.Weapon;
+import java.util.ArrayList;
+import java.util.List;
+import utils.Dice;
 
 /**
  *
@@ -36,9 +39,31 @@ public class Quest {
         player.addInventory(lettre);
         Display.winItem(lettre);
         Display.mainQuest2();
-        Armor armureDeBase = new Armor("armureDeBase",10,1,5);
-        character.Character courgette = new character.Monster(1, 2, 2, 2, 5, "Courgette", (Controler)new ControlerIA(),armureDeBase ,new Weapon[2]);
-        Fight fight = new Fight(player,courgette);
+        
+        for(int i=0;i<10;i++){
+            Display.mainQuest3();
+            int av = Dice.roll(5+2*i, 10+2*i);
+            Armor armureDeBase = new Armor("armureDeBase",10,1,av);
+            int dmgmin = Dice.roll(5+2*i, 10+2*i);
+            int dmgmax = Dice.roll(10+2*i, 15+2*i);
+            Weapon épéeDeBase = new Weapon("épéeDeBase",10,1,dmgmax,dmgmin);            
+            int force =Dice.roll(2+2*i, 7+2*i);
+            int dexterity =Dice.roll(1+2*i, 3+2*i);
+            int defence =Dice.roll(1+2*i, 3+2*i);
+            int health =Dice.roll(5+2*i, 10+2*i);
+            int hasard = Dice.roll(1, 2);
+            if(hasard==1){
+                character.Character courgette = new character.Monster(1, force, dexterity, defence, health, "Courgette", (Controler)new ControlerIA(),armureDeBase ,new Weapon[2]);
+                Fight fight = new Fight(player,courgette);
+            }
+            else{
+                Weapon[] weapons ={épéeDeBase,null};
+                character.Character courgette = new character.Monster(1, force, dexterity, defence, health, "Courgette", (Controler)new ControlerIA(),(Armor) null,weapons);
+                Fight fight = new Fight(player,courgette);
+
+            }
+            
+        }
 
     
             
